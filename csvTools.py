a0bfe02d6fdf0a51ded6a6eaa07ba1ed2d57e2ac
Adam <adam@leibel.net>
2016-08-31 10:21:53 -0700

    renamed csv_tools.py to csvTools.py

--- csvTools.py
+++ csvTools.py
@@ -1,11 +1,11 @@
-#adapted from http://stackoverflow.com/a/34715816
-
 import os
 import glob
 import csv
 from xlsxwriter.workbook import Workbook
 
-def csv_merge(directory, deliminator=','):
+#for a directory of csv files, create single xlsx file with each csv as a worksheet
+#adapted from http://stackoverflow.com/a/34715816
+def csvMergeToXlsx(directory, deliminator=','):
     workbook = Workbook(os.path.join(directory, os.path.basename(directory)) + ".xlsx", {'strings_to_numbers': True})
     for csvfile in glob.glob(os.path.join(directory, '*.csv')):
         worksheet = workbook.add_worksheet(os.path.splitext(os.path.basename(csvfile))[0])
@@ -16,3 +16,4 @@ def csv_merge(directory, deliminator=','):
                 for colNum, colText in enumerate(splitLine):
                     worksheet.write(rowNum, colNum, colText)
     workbook.close()
+
