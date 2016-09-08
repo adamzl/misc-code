063f5c1e5fa945f906aace46a41ab75355471e04
Adam <adam@leibel.net>
2016-09-08 13:06:32 -0700

    added cyclestatsCleanup droplet

---
+++ droplets/nvidia/cyclestatsCleanup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import re
+import csv
+
+def cyclestatsCleanup(inPath, outDir=""):
+    if outDir == "":
+        outDir = inPath[:-4] + ".csv"
+    else:
+        nameNoExt = os.path.split(os.path.splitext(inPath)[0])[1]
+        outDir = os.path.join(outDir, nameNoExt) + ".csv"
+    with open(inPath, 'r') as inFile:
+        with open(outDir, 'w') as outFile:
+            csvReader = csv.reader(inFile, dialect='excel-tab')
+            #throw away header components and write of column headers
+            for line in csvReader:
+                match = re.match('\[#id\]', line[0])
+                if not match:
+                    continue
+                _writeListToCsvFile(outFile, line)
+                break
+            #transcribe rows of data, PRESUMED FIVE COLUMNS
+            for line in csvReader:
+                match = re.match(r'\d+', line[0])
+                if not match:
+                    break
+                _writeListToCsvFile(outFile, line)
+
+def _writeListToCsvFile(outFile, line):
+    for count, cell in enumerate(line):
+        if count > 0:
+            outFile.write(',')
+        outFile.write(cell)
+    outFile.write('\n')
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        inPath = sys.argv[1]
+        print("[1/2] inPath: {}".format(inPath))
+    else:
+        inPath = input("[1/2] inPath: ")
+    outDir = input("[2/2] outDir: ")
+    cyclestatsCleanup(inPath, outDir)
