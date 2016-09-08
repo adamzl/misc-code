703d82ae00c7bb47bcd7b5a618d3cbf74422f77f
Adam <adam@leibel.net>
2016-09-08 16:43:28 -0700

    adding two more droplets

---
+++ droplets/fileSystemOrganization/regexRename.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import re
+import shutil
+
+# will rename files by using only those that match regex.
+# new name is the concatination of the regex groups
+def regexRename(inDir, regexPattern):
+    for file in os.listdir(inDir):
+        match = re.match(regexPattern, file)
+        if match:
+            newFilename = ""
+            for group in match.groups():
+                newFilename = newFilename + group
+            shutil.move(os.path.join(inDir, file), os.path.join(inDir, newFilename))
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        inDir = sys.argv[1]
+        print("[1/2] inDir: {}".format(inDir))
+    else:
+        inDir = input("[1/2] inDir: ")
+    regexPattern = input("[2/2] regexPattern: ")
+    regexRename(inDir, regexPattern)
---
+++ droplets/nvidia/cyclestatsStateBreakout.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import re
+import csv
+
+#assumed cyclcestatsCleanup was run first
+#group(1) of pattern matches are transcribed as the data of each column
+#common values for listOfRePatterns: ["pshader.*appHash=(0x[\dA-F`]+)", "compute.*appHash=(0x[\dA-F`]+)"]
+def cyclestatsStateBreakout(inPath, listOfRePatterns=[], outDir=""):
+    if outDir == "":
+        outDir = os.path.split(inPath)[0]
+    with open(inPath, 'r') as inFile:
+        outPath = os.path.join(outDir, "_" + os.path.split(inPath)[1])
+        with open(outPath, 'w') as outFile:
+            csvReader = csv.reader(inFile)
+            for line in csvReader:
+                if line[0] == "[#id]":
+                    for pattern in listOfRePatterns:
+                        line.append(pattern)
+                else:
+                    stateString = line[-1]
+                    for pattern in listOfRePatterns:
+                        match = re.search(pattern, stateString)
+                        if match != None:
+                            line.append(match.group(1))
+                _writeListToCsvFile(outFile, line)
+
+def _writeListToCsvFile(outFile, line):
+    for count, cell in enumerate(line):
+        if count > 0:
+            outFile.write(',')
+        outFile.write(cell)
+    outFile.write('\n')
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        inPath = sys.argv[1]
+        print("[1/2] inPath: {}".format(inPath))
+    else:
+        inPath = input("[1/2] inPath: ")
+    regexPattern = input("[2/2] regexPattern: ")
+    cyclestatsStateBreakout(inPath, [regexPattern])
