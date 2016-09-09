08da9f9d670fa9b9aed4022035f0cccc8c764bf5
Adam <adam@leibel.net>
2016-09-09 10:38:52 -0700

    cyclestatsStateBreakout accepts lists of patterns on command line or predefined patterns. also moves [state] to last column

--- droplets/nvidia/cyclestatsStateBreakout.py
+++ droplets/nvidia/cyclestatsStateBreakout.py
@@ -15,14 +15,17 @@ def cyclestatsStateBreakout(inPath, listOfRePatterns=[], outDir=""):
             csvReader = csv.reader(inFile)
             for line in csvReader:
                 if line[0] == "[#id]":
-                    for pattern in listOfRePatterns:
-                        line.append(pattern)
+                    patternsInsertIndex = len(line) - 1
+                    line = line[:patternsInsertIndex] + listOfRePatterns + line[patternsInsertIndex:]
                 else:
-                    stateString = line[-1]
+                    patternMatches = []
                     for pattern in listOfRePatterns:
-                        match = re.search(pattern, stateString)
+                        match = re.search(pattern, line[-1])
                         if match != None:
-                            line.append(match.group(1))
+                            patternMatches.append(match.group(1))
+                        else:
+                            patternMatches.append("")
+                    line = line[:patternsInsertIndex] + patternMatches + line[patternsInsertIndex:]
                 _writeListToCsvFile(outFile, line)
 
 def _writeListToCsvFile(outFile, line):
@@ -32,6 +35,10 @@ def _writeListToCsvFile(outFile, line):
         outFile.write(cell)
     outFile.write('\n')
 
+predefinedRegexPatterns = [
+    ("[tags,pshader,compute]", ["^([A-Z_<>]+)", "pshader.*appHash=(0x[\dA-F`]+)", "compute.*appHash=(0x[\dA-F`]+)"])
+]
+
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         inPathList = sys.argv[1:]
@@ -40,7 +47,15 @@ if __name__ == "__main__":
             print("\t{}".format(arg))
     else:
         inPath = input("[1/2] inPath: ")
-        inPathList = [inPath] 
-    regexPattern = input("[2/2] regexPattern: ")
+        inPathList = [inPath]
+    print("[2/2] regexPattern: enter a single expression or ^^#")
+    for count,pattern in enumerate(predefinedRegexPatterns):
+        print("\t{0}: {1}".format(count, pattern[0]))
+    inputPatternText = input("      regexPattern: ")
+    match = re.fullmatch("\^\^(\d+)", inputPatternText)
+    if match:
+        regexPattern = predefinedRegexPatterns[int(match.group(1))][1]
+    else:
+        regexPattern = [inputPatternText]
     for inPath in inPathList:
-        cyclestatsStateBreakout(inPath, [regexPattern])
+        cyclestatsStateBreakout(inPath, regexPattern)
