2f86a0b89d7a13ed794bc8ef798101f928140dfa
Adam <adam@leibel.net>
2016-09-14 10:45:15 -0700

    fixup to workflow.processCyclestatsSurvey()

--- python/azlpy/workflow.py
+++ python/azlpy/workflow.py
@@ -2,25 +2,29 @@ import glob
 import shutil
 import os
 
-import apic
 import csvTools
-import nvCsvTools
-import mapMethods
+import nvidia
+import fileSystem
 
 # cyclestats survey to cleaned up folder
-def processCyclestatsSurvey(originalData, outDir):
+def processCyclestatsSurvey(inDir, outDir):
     # get all the *.xls files from their directories that were named after the APIC folder
+    stageInDir = inDir
     stageOutDir = os.path.join(outDir, "1_renameFiles")
     os.makedirs(stageOutDir, exist_ok=True)
-    fileList = glob.glob(os.path.join(originalData, r"**", r"*"))
-    a = [mapMethods.moveFileRenameToOriginFolder(inPath, stageOutDir) for inPath in fileList]
+    fileList = glob.glob(os.path.join(stageInDir, r"**\*.xls"))
+    [fileSystem.moveFileRenameToOriginFolder(inPath, stageOutDir) for inPath in fileList]
 
     # clean all the xls files into csv files with only event data
+    stageInDir = stageOutDir
     stageOutDir = os.path.join(outDir, "2_cleanAndConvertToCsv")
     os.makedirs(stageOutDir, exist_ok=True)
-    fileList = glob.glob(os.path.join(outDir, "1_renameFiles", r"*"))
-    a = [nvCsvTools.cyclestatsCleanup(inPath, stageOutDir, True) for inPath in fileList]
+    fileList = glob.glob(os.path.join(stageInDir, r"*"))
+    for inPath in fileList:
+        outPath = os.path.join(stageOutDir, os.path.split(inPath)[1][:-4] + ".csv")
+        nvidia.cyclestatsCleanup(inPath, outPath)
 
     # create multifileSum.csv
+    stageInDir = stageOutDir
     stageOutPath = os.path.join(outDir, "multifileSum.csv")
-    csvTools.multifileSum(stageOutDir, 'C', stageOutPath)
+    csvTools.multifileSum(stageInDir, stageOutPath, 'C')
--- python/multiFolderToMultiFile.py
+++ python/multiFolderToMultiFile.py
@@ -1,16 +1,19 @@
 import sys
+import os.path
+import glob
 import azlpy.fileSystem
 
 if __name__ == "__main__":
     try:
-        if len(sys.argv) >= 2:
+        if len(sys.argv) == 2:
             inDir = sys.argv[1]
             print("[1/3] inDir: {}".format(inDir))
         else:
             inDir = input("[1/3] inDir: ")
         outDir = input("[2/3] outDir: ")
         regexFilePattern = input("[3/3] regexFilePattern: ")
-        azlpy.fileSystem.multiFolderToMultiFile(inDir, regexFilePattern, outDir)
+        fileList = glob.glob(os.path.join(inDir, regexFilePattern))
+        [azlpy.fileSystem.moveFileRenameToOriginFolder(inPath, outDir) for inPath in fileList]
     except Exception as e:
         print(e)
         input("press any key")
