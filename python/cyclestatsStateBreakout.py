dc244cad646a546f415940432522b7edf50b5970
Adam <adam@leibel.net>
2016-09-15 14:09:38 -0700

    cyclestats breakout for all shader stages

--- python/cyclestatsStateBreakout.py
+++ python/cyclestatsStateBreakout.py
@@ -4,7 +4,16 @@ import azlpy.nvidia
 
 predefinedRegexPatterns = [
     ("[tags, pshader, compute]", ["^([A-Z_<>]+)", "pshader.*appHash=(0x[\dA-F`]+)", "compute.*appHash=(0x[\dA-F`]+)"]),
-    ("[tags, pshader.apphash, pshader.ucodehash, compute.apphash, compute.ucodehash]", ["^([A-Z_<>]+)", "pshader.*appHash=(0x[\dA-F`]+)", "pshader.*ucodeHash=(0x[\dA-F`]+)", "compute.*appHash=(0x[\dA-F`]+)", "compute.*ucodeHash=(0x[\dA-F`]+)"]),
+    ("[tags, pshader.apphash, pshader.ucodehash, compute.apphash, compute.ucodehash]", ["^([A-Z_<>]+)",
+                                                                                        "pshader.*appHash=(0x[\dA-F`]+)", "pshader.*ucodeHash=(0x[\dA-F`]+)",
+                                                                                        "compute.*appHash=(0x[\dA-F`]+)", "compute.*ucodeHash=(0x[\dA-F`]+)"]),
+    ("[tags, pshader/compute/vshader/gshader/hullshader/domainshader.appHash/ucodeHash]", ["^([A-Z_<>]+)",
+                                                                                           "pshader.*appHash=(0x[\dA-F`]+)", "pshader.*ucodeHash=(0x[\dA-F`]+)",
+                                                                                           "compute.*appHash=(0x[\dA-F`]+)", "compute.*ucodeHash=(0x[\dA-F`]+)",
+                                                                                           "vshader.*appHash=(0x[\dA-F`]+)", "vshader.*ucodeHash=(0x[\dA-F`]+)",
+                                                                                           "gshader.*appHash=(0x[\dA-F`]+)", "gshader.*ucodeHash=(0x[\dA-F`]+)",
+                                                                                           "hullshader.*appHash=(0x[\dA-F`]+)", "hullshader.*ucodeHash=(0x[\dA-F`]+)",
+                                                                                           "domainshader.*appHash=(0x[\dA-F`]+)", "domainshader.*ucodeHash=(0x[\dA-F`]+)"]),
 ]
 
 if __name__ == "__main__":
